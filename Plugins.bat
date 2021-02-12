f93aec0d42af03525e1168f7bffa1bb4b724f3c9
adawsonelli <dawsonelli@wisc.edu>
2021-02-11 22:34:53 -0600

    building pantograph class

--- Plugins.bat
+++ Plugins.bat
@@ -1,15 +1,17 @@
+:: install the NL_OPT package from the prebuild .whl file stored locally
+pip install ./wheels/NLopt-2.7.0-cp37-cp37m-win_amd64.whl
 
 :: install plotly extension
 jupyter labextension install jupyterlab-plotly@4.14.3 --no-build
 
-::install ipywidgets extension (for interactivity)
+:: install ipywidgets extension (for interactivity)
 jupyter labextension install @jupyter-widgets/jupyterlab-manager plotlywidget@4.14.3 --no-build
 
-::install debugger support
+:: install debugger support
 jupyter labextension install @jupyterlab/debugger --no-build
 
-::rebuild jupyterlab
+:: rebuild jupyterlab
 jupyter lab build
 
-::list the installed extensions
+:: list the installed extensions
 jupyter labextension list
